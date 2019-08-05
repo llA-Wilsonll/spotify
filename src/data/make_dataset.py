6431e439a89c650696587827f70a4fca50846a15
MathCaptain <a-wilson@hotmail.com.au>
2019-08-05 18:55:41 +1000

    👌 IMRPOVE: Style Cleanup & distinct check

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,9 +6,8 @@ I want to analyse. More specifically:
 """
 
 import requests  # Used for API calls
-import json  # To parse response from API call
 from spfy_token import AccessToken  # Module where I store my access token
-from pandas import DataFrame, concat
+import pandas as pd  # DataFrame, concat, crosstab
 
 # Instructions on how to obtain Auth Token can be found in docs directory
 access_token = AccessToken.TOKEN
@@ -27,12 +26,17 @@ countries = ["AU", "US", "GB", "CN", "JP", "BR", "DE", "ZA", "SE", "SG"]
 # Initialising a dictionary to store track data for each country
 tracks_dict = {}
 
-keys = ['name', 'duration_ms','popularity']  # List of keys we want from the api response
-extract = lambda x, y: dict(zip(x, map(y.get, x)))  # Function to extract subset of key-value pairs from a dict
+keys = ['name', 'duration_ms', 'popularity']  # List of keys we want from the api response
+
+
+# Function to extract subset of key-value pairs from a dict
+def extract(x, y):
+    return dict(zip(x, map(y.get, x)))
+
 
 # Looping through each country and extracting the top 10 songs
 for country in countries:
-    #  Parameters to URL Endpoint
+    # Parameters to URL Endpoint
     parameters = {"id": artist_id, "country": country}
 
     # Headers for URL Endpoint
@@ -50,12 +54,16 @@ for country in countries:
 
     # Taking a subset of the keys
     for i in range(len(tracks_dict[country])):
-        tracks_dict[country][i] = extract(keys,tracks_dict[country][i])
+        tracks_dict[country][i] = extract(keys, tracks_dict[country][i])
 
     # Changing from list to DataFrame
-    tracks_dict[country] = DataFrame(tracks_dict[country])
+    tracks_dict[country] = pd.DataFrame(tracks_dict[country])
 
 # Concatenating dict of country DataFrames into one DataFrame
-tracks_df = concat(tracks_dict,sort=False)
+tracks_df = pd.concat(tracks_dict, sort=False)
 tracks_df.reset_index(inplace=True)
 tracks_df.columns = ['country', 'number'] + keys
+
+# Inspecting to see if country data sets are actually different
+cross_tab = pd.crosstab(tracks_df.name, tracks_df.number).apply(lambda r: r / r.sum(), axis=1)
+print(cross_tab)
