feeed6b5a9b442a61f440c50506c8089911e8c90
MathCaptain <a-wilson@hotmail.com.au>
2019-09-30 07:48:29 +1000

    📦 NEW: add cluster code

--- src/data/spfy_token.py
+++ src/data/spfy_token.py
@@ -3,6 +3,6 @@ Location to store my spotify token details
 """
 
 class AccessToken:
-    TOKEN = ""
-    REFRESH_TOKEN = ""
+    TOKEN = "BQD__kRXEglUXAE2ZLbkWXiWNnSFrl44s4Z9EyWazzrPwdW9kC34EcbFNpRk8TY8LLPX3MWibBkBVsgc18PhjDYkuFe4MItn6WwT1B81hkNdFnkwQkpPiRWB_Mhd27KLyNL10gV-et1LAdomgkI"
+    REFRESH_TOKEN = "AQDb-ekzFSOoK3_3_iWd3FydtN35jmnk0K94WMejH47Sp5b2keoPlIVaXCKs-4cijq-Hanh-IAiQrteevfYjA9PCUoiaiQsti7_D2hCQtwd5yib1bxz3QwUiRZAdW0W4P4qdCQ"
       
